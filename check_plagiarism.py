f14a69ae34f0776f1c6fe2b14e4298640aecaa29
Jickx <63093696+Jickx@users.noreply.github.com>
2022-09-09 14:15:16 +0300

    refactor

--- check_plagiarism.py
+++ check_plagiarism.py
@@ -1,29 +1,28 @@
-def check_plagiarism(alice, zeliboba):
-    alice = list(alice)
-    zeliboba = list(zeliboba)
+from collections import Counter
+
+
+def check_plagiarism(alice, zeliboba) -> str:
+    alice, zeliboba = list(alice), list(zeliboba)
+    alice_ctr = dict(Counter(alice))
     result = [None for i in range(len(alice))]
     i = 0
     while i < len(alice):
         if zeliboba[i] == alice[i]:
+            alice_ctr[alice[i]] -= 1
             alice[i], zeliboba[i] = None, None
             result[i] = 'P'
             i += 1
             continue
         i += 1
-    i = 0
-    while i < len(alice):
+    for i in range(len(zeliboba)):
         if not zeliboba[i]:
-            i += 1
             continue
-        if zeliboba[i] in alice:
-            letter_index = alice.index(zeliboba[i])
-            alice[letter_index] = None
+        if zeliboba[i] in alice_ctr and alice_ctr[zeliboba[i]] > 0:
+            alice_ctr[zeliboba[i]] -= 1
             zeliboba[i] = None
             result[i] = 'S'
         else:
             result[i] = 'I'
-            zeliboba[i] = None
-        i += 1
     return ''.join(result)
 
 
@@ -37,5 +36,5 @@ def check_plagiarism(alice, zeliboba):
 
 
 assert check_plagiarism('CLOUD', 'CUPID') == 'PSIIP'
-# assert check_plagiarism('ALICE', 'ELIBO') == 'SPPII'
-# assert check_plagiarism('ABCBCYA', 'ZBBACAA') == 'IPSSPIP'
+assert check_plagiarism('ALICE', 'ELIBO') == 'SPPII'
+assert check_plagiarism('ABCBCYA', 'ZBBACAA') == 'IPSSPIP'
