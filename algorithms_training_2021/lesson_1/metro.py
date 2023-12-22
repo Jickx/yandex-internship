e555e4d91b7780e4422a661301d029f952d36839
Jickx <valentin.sharonov@gmail.com>
2023-12-22 13:07:32 +0300

    add metro.py

---
+++ algorithms_training_2021/lesson_1/metro.py
@@ -0,0 +1,34 @@
+import os
+
+
+def count_trains(a: int, b: int, n: int, m: int) -> None:
+    a_min = n + a * (n - 1)
+    a_max = n + a * (n + 1)
+    b_min = m + b * (m - 1)
+    b_max = m + b * (m + 1)
+
+    t_min = a_min if a_min > b_min else b_min
+    t_max = a_max if a_max < b_max else b_max
+
+    if t_min > t_max:
+        print('-1')
+    else:
+        print(t_min, t_max)
+
+
+path = os.path.join('input', __name__)
+with open(path, 'r') as f:
+    input_lines = f.read().strip().split('\n\n\n')
+
+
+def test_count_trains(capfd):
+    for line in input_lines:
+        numbers, expected = line.split('\n\n')
+        message = numbers.replace('\n', ' ')
+        a, b, n, m = map(int, numbers.split('\n'))
+        count_trains(a, b, n, m)
+        actual, err = capfd.readouterr()
+        assert actual.strip() == expected, message
+
+
+# count_trains(*map(int, [input(), input(), input(), input()]))
