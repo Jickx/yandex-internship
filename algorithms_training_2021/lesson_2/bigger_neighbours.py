6219a038939cdcb6e89a590e2d36b85f932e2348
Jickx <valentin.sharonov@gmail.com>
2023-12-24 13:27:45 +0300

    add bigger_neighbours.py

---
+++ algorithms_training_2021/lesson_2/bigger_neighbours.py
@@ -0,0 +1,21 @@
+from algorithms_training_2021.lesson_2.get_input import get_input
+
+
+def find_bigger_neighbours(arr: list[int]) -> int:
+    ctr = 0
+    for i in range(1, len(arr) - 1):
+        if arr[i] > arr[i + 1] and arr[i] > arr[i - 1]:
+            ctr += 1
+    return ctr
+
+
+# print(find_bigger_neighbours(list(map(int, input().split()))))
+
+def test_biggest_neighbours(capfd):
+    input_lines = get_input(__name__)
+    for line in input_lines:
+        numbers, expected = line.split('\n\n')
+        message = numbers.replace('\n', ' ')
+        print(find_bigger_neighbours(list(map(int, numbers.split()))))
+        actual, err = capfd.readouterr()
+        assert actual.strip() == expected, message
