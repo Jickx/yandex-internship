e72754e8737f4ccffce6f5c2db9bb93010b505ab
Jickx <valentin.sharonov@gmail.com>
2023-12-24 12:57:36 +0300

    refactor functional implementation

--- algorithms_training_2021/lesson_2/nearest_number.py
+++ algorithms_training_2021/lesson_2/nearest_number.py
@@ -1,27 +1,22 @@
 from algorithms_training_2021.lesson_2.get_input import get_input
 
 
-def find_nearest_number(nums: list[int], num: int) -> None:
-    nearest = 0
-    nearest_diff = abs(num - nums[nearest])
-    for i in range(1, len(nums)):
-        if abs(num - nums[i]) < nearest_diff:
-            nearest = i
-            nearest_diff = abs(num - nums[i])
-    print(nums[nearest])
+def find_closest_number(arr, num):
+    print(min(arr, key=lambda el: abs(num - el)))
 
 
-i = input()
-nums = list(map(int, input().split()))
-num = int(input())
-find_nearest_number(nums, num)
+# N = int(input())
+# arr = list(map(int, input().split()))
+# num = int(input())
+#
+# print(find_closest_number(arr, num))
 
-# def test_find_nearest_number(capfd):
-#     input_lines = get_input(__name__)
-#     for line in input_lines:
-#         numbers, expected = line.split('\n\n')
-#         message = numbers.replace('\n', ' ')
-#         i, nums, num = numbers.split('\n')
-#         find_nearest_number(list(map(int, nums.split())), int(num))
-#         actual, err = capfd.readouterr()
-#         assert actual.strip() == expected, message
+def test_find_closest_number(capfd):
+    input_lines = get_input(__name__)
+    for line in input_lines:
+        numbers, expected = line.split('\n\n')
+        message = numbers.replace('\n', ' ')
+        i, arr, num = numbers.split('\n')
+        find_closest_number(list(map(int, arr.split())), int(num))
+        actual, err = capfd.readouterr()
+        assert actual.strip() == expected, message
